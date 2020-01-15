b2baf836374c704823ca269100038e8750829f36
stcoyle137 <22sc01@cf.k12.mn.us>
2020-01-14 20:51:39 -0600

    Start a subsystem for Neo Motors

--- src/main/java/frc/robot/subsystems/NeoMotorController.java
+++ src/main/java/frc/robot/subsystems/NeoMotorController.java
@@ -6,23 +6,94 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
-import frc.robot.Constants;
 import frc.robot.Constants.NeoMotorControllerConstants;
+
 public class NeoMotorController extends SubsystemBase {
 
   private CANSparkMax m_motor;
-  /**
-   * Creates a new NeoMotorController.
-   */
-  public NeoMotorController() {
+  private CANEncoder m_encoder;
+  private CANPIDController m_PIDContoller;
+  private double PIDMinOutput = -1.0;
+  private double PIDMaxOutput = 1.0;
+  private int maxRPM = 5700;
+  private PIDConstants PID;
+
+  public NeoMotorController(PIDConstants PID) {
     m_motor = new CANSparkMax(NeoMotorControllerConstants.CANid, NeoMotorControllerConstants.motorType);
+    m_motor.restoreFactoryDefaults();
+
+    m_PIDContoller = m_motor.getPIDController();
+
+    this.PID = PID;
+    m_PIDContoller.setP(this.PID.kP);
+    m_PIDContoller.setI(this.PID.kI);
+    m_PIDContoller.setD(this.PID.kD);
+    m_PIDContoller.setIZone(this.PID.kIz);
+    m_PIDContoller.setFF(this.PID.kFF);
+    m_PIDContoller.setOutputRange(PIDMinOutput, PIDMaxOutput);
+
+    m_encoder = m_motor.getEncoder();
+  }
+
+  public double getPIDMinOutput() {
+    return this.PIDMinOutput;
+  }
+
+  public void setPIDMinOutput(double PIDMinOutput) {
+    this.PIDMinOutput = PIDMinOutput;
+  }
+
+  public double getPIDMaxOutput() {
+    return this.PIDMaxOutput;
+  }
+
+  public void setPIDMaxOutput(double PIDMaxOutput) {
+    this.PIDMaxOutput = PIDMaxOutput;
+  }
+
+  public int getMaxRPM() {
+    return this.maxRPM;
+  }
+
+  public void setMaxRPM(int maxRPM) {
+    this.maxRPM = maxRPM;
+  }
+
+  public PIDConstants getPID() {
+    return this.PID;
+  }
+
+  public void setPID(PIDConstants PID) {
+    this.PID = PID;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public class PIDConstants{
+    public double kP;
+    public double kI;
+    public double kD;
+    public double kIz;
+    public double kFF;
+    
+    public PIDConstants(double kP, double kI, double kD, double kIz, double kFF){
+      this.kP = kP;
+      this.kI = kI;
+      this.kD = kD;
+      this.kIz = kIz;
+      this.kFF = kFF;
+    }
+
+    public PIDConstants(double kP, double kI, double kD){
+      this(kP, kI, kD, 0, 0);
+    }
   }
 }
